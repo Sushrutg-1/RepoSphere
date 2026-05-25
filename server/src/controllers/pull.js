e98ecea75bbc8fe82df4aa64d7e179c1c6a3142b
Sushrut Gondane <sushrutg.1@gmail.com>
2026-05-25 22:40:04 +0530

    Implements Pull and Revert command logic

--- server/src/controllers/pull.js
+++ server/src/controllers/pull.js
@@ -1,3 +1,57 @@
-export async function pullRepo() {
-  console.log("Pull repo command run");
-}
+import path from "path";
+import fs from "fs";
+import storage from "../config/supabse.config.js";
+
+export const pullRepo = async () => {
+  const repoPath = path.resolve(process.cwd(), ".reposphere");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    // lists of all commit folders (folder name = commit ID)
+    const { data: commitFolders, error } = await storage
+      .from("reposphere")
+      .list("commits");
+
+    if (error) {
+      console.log("Error while getting commit folder : ", error.message);
+    }
+
+    for (const folder of commitFolders) {
+      const commitId = folder.name;
+
+      //lists of  all files inside each commit folder
+      const { data: files, error } = await storage
+        .from("reposphere")
+        .list(`commits/${commitId}`);
+
+      if (error) {
+        console.log("Error while getting folder : ", error.message);
+      }
+
+      for (const file of files) {
+        const filePath = `commits/${commitId}/${file.name}`;
+
+        //for downloading each file from one folder at a time
+        const { data: fileData, error } = await storage
+          .from("reposphere")
+          .download(filePath);
+
+        if (error) {
+          console.log("Error while downloading files : ", error.message);
+        }
+
+        //converting blob data into binary
+        const buffer = Buffer.from(await fileData.arrayBuffer());
+
+        //local path for downloading file
+        const commitDir = path.join(commitsPath, commitId);
+        await fs.promises.mkdir(commitDir, { recursive: true });
+        await fs.promises.writeFile(path.join(commitDir, file.name), buffer);
+      }
+    }
+
+    console.log("All commits pulled successfully.");
+  } catch (error) {
+    console.error("Error while pulling commit from cloud : ", error.message);
+  }
+};
--- server/src/controllers/revert.js
+++ server/src/controllers/revert.js
@@ -1,3 +1,25 @@
+import fs from "fs";
+import path from "path";
+import util from "util";
+
+const readDir = util.promisify(fs.readdir);
+const copyFile = util.promisify(fs.copyFile);
+
 export const revertRepo = async (commitId) => {
-  console.log("revert repo command run");
+  const repoPath = path.resolve(process.cwd(), ".reposphere");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    const commitDir = path.join(commitsPath, commitId);
+    const files = await readDir(commitDir);
+    const parentPath = path.join(repoPath, "..");
+
+    for (const file of files) {
+      await copyFile(path.join(commitDir, file), path.join(parentPath, file));
+    }
+
+    console.log(`Commit ${commitId} reverted successfully!`);
+  } catch (error) {
+    console.error("Error while reverting commit : ", error.message);
+  }
 };
